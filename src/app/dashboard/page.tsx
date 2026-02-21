0e1783ed2b33cbfc79cefe21ffd5286a599d9844
Daniel <danielau8807@gmail.com>
2026-02-21 23:48:47 +1100

    basic dashboard layout with lock in button and schedule select

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import ScheduleSelector from "@/src/components/schedule-select";
+import LogWorkout from "@/src/components/log-workout";
 
 export default function DashboardPage() {
   return (
-    <div>
+    <div className="flex flex-col gap-10 items-center p-10 w-auto">
+      <LogWorkout />
       <ScheduleSelector />
     </div>
   );
