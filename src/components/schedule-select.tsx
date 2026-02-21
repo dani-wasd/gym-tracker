d8cd1d5ec9ff86147baae3296d2bacc95c8a7fc6
Daniel <danielau8807@gmail.com>
2026-02-21 23:48:00 +1100

    added new animation for lock in button

--- src/components/schedule-select.tsx
+++ src/components/schedule-select.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Day } from "@/src/definitions";
+import { Day } from "@/src/lib/definitions";
 import {
   GlowCard, GlowCardHeader, GlowCardContent, GlowCardFooter,
   GlowCardDescription,
@@ -31,10 +31,10 @@ export default function ScheduleSelector() {
 
   // Todo: Maybe set max width for this card add gradient for button line
   return (
-    <GlowCard className="text-center">
-      <GlowCardHeader className="text-lg font-bold">Week Planner</GlowCardHeader>
+    <GlowCard className="text-center min-w-full">
+      <GlowCardHeader className="text-lg font-bold pb-1">Week Planner</GlowCardHeader>
       <GlowCardContent className="flex flex-col justify-center gap-2 py-4">
-        <div className="flex flex-row gap-2 justify-center">
+        <div className="flex flex-row flex-wrap gap-2 justify-center">
           {daysOfWeek.map((day, index) => (
             <CircularButton
               key={day}
@@ -54,14 +54,37 @@ export default function ScheduleSelector() {
         <GlowCardDescription>Selected Dates: {daysOfWeek.filter((_, index) => isDaySelected(index)).join(", ")}</GlowCardDescription>
       </GlowCardContent>
       <GlowCardFooter className="flex justify-center pt-2">
-        <Button
-          variant="outline"
-          size="md"
-          onClick={toggleLockStatus}
-          className="px-8"
-        >
-          {isLocked ? "Change Schedule" : "Lock In Schedule"}
-        </Button>
+        <style>{`
+          @keyframes rotate {
+            100% {
+              transform: rotate(1turn);
+            }
+          }
+      
+          .animatedOutline::before {
+            content: '';
+            position: absolute;
+            z-index: -2;
+            left: -50%;
+            top: -50%;
+            width: 200%;
+            height: 200%;
+            background-position: 100% 50%;
+            background-repeat: no-repeat;
+            background-size: 50% 30%;
+            filter: blur(6px);
+            background-image: linear-gradient(#FFF);
+            animation: rotate 4s linear infinite;
+          }
+        `}</style>
+        <div className={`${isLocked ? "" : "animatedOutline"} relative z-0 bg-white/15 overflow-hidden p-0.5 flex items-center justify-center rounded-full hover:scale-105 transition duration-300 active:scale-100`} >
+          <button
+            onClick={toggleLockStatus}
+            className="px-8 text-sm py-3 text-white rounded-full font-medium bg-gray-900/80 backdrop-blur"
+          >
+            {isLocked ? "Change Schedule" : "Lock In Schedule"}
+          </button>
+        </div>
       </GlowCardFooter>
     </GlowCard>
   );
