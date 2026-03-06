3e2af54d9151952e40c5d1965e9a5ea45a93a524
Daniel <danielau8807@gmail.com>
2026-03-06 19:18:18 +1100

    the muscleGroup type is now singular

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,1 +1,2 @@
 export type Day = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat' | 'Sun';
+export type muscleGroup = 'Chest' | 'Back' | 'Shoulders' | 'Biceps' | 'Triceps' | 'Legs' | 'Abs' | 'Full Body';
