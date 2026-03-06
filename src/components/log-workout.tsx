c2f2409630311d72889420a1fdcb8d87e6e2008d
Daniel <danielau8807@gmail.com>
2026-03-06 19:24:59 +1100

    added select muscle modal and submit workout logic

--- src/components/log-workout.tsx
+++ src/components/log-workout.tsx
@@ -1,61 +1,102 @@
 "use client";
 
-import { GlowCard, GlowCardHeader, GlowCardContent, GlowCardDescription } from "./ui/glow-card";
+import { GlowCard, GlowCardHeader, GlowCardContent, GlowCardDescription } from "@/src/components/ui/glow-card";
+import { WorkoutTimer } from "@/src/components/timer";
+import { MusclesSelection } from "@/src/components/muscles-selection";
 import CircularButton from "@/src/components/ui/circular-button";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { saveWorkout } from "@/src/app/dashboard/actions";
 
-export default function LogWorkout() {
+export default function LogWorkout(userId: {userId: string}) {
   const [isWorkingOut, setIsWorkingOut] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
-  const toggleWorkout = () => {
-    setIsWorkingOut(prev => !prev);
-  }
+  const userIdString: string = userId.userId
 
-  return (
-    <GlowCard className="min-w-full text-center">
-      <GlowCardHeader className="pb-0">
-        {isWorkingOut ? (<WorkoutTimer/>) : ""}
-      </GlowCardHeader>
-
-      <GlowCardContent className="pt-[24px] flex flex-col justify-center items-center">
-        <CircularButton 
-          bgColor={isWorkingOut ? "white" : "none"}
-          outlineColor={isWorkingOut ? "black" : "white"}
-          className={`${isWorkingOut ? "text-black" : ""} w-50 h-50`}
-          onClick={() => toggleWorkout()}
-        >
-          {isWorkingOut ? "End" : "Workout"}
-        </CircularButton>
-
-        <GlowCardDescription className="pt-5">
-          <a>Done your workout already? Log it</a>
-        </GlowCardDescription>
-      </GlowCardContent>
-    </GlowCard>
-  )
-}
-
-function WorkoutTimer() {
-  const [seconds, setSeconds] = useState(0);
+  // Open/Close modal
+  useEffect(() => {
+    if (isModalOpen) {
+      dialogRef.current?.showModal();
+    } 
+    else {
+      dialogRef.current?.close();
+    }
+  }, [isModalOpen]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds((s) => s + 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+    const isWorkingOut = localStorage.getItem("isWorkingOut");
+    if (isWorkingOut === "true") {
+      setIsWorkingOut(true);
+    }
+  }, [])
 
-  const formatTime = (s: number) => {
-    const mins = Math.floor(s / 60);
-    const secs = s % 60;
-    return `${mins}:${secs.toString().padStart(2, "0")}`;
+  const toggleWorkout = () => {
+    const nextState = !isWorkingOut;
+
+    setIsWorkingOut(nextState);
+    localStorage.setItem("isWorkingOut", nextState.toString());
+
+    if (nextState) {
+      localStorage.setItem("startTime", new Date().toISOString());
+    }
+    else {
+      setIsModalOpen(true);
+    }
   };
 
+  const handleFinishWorkout = async (muscles: string[]) => {
+    const startTime = localStorage.getItem("startTime");
+    const endTime = new Date().toISOString();
+
+    try {
+      await saveWorkout(userIdString, {
+        startTime,
+        endTime,
+        localDate: new Date().toISOString().split("T")[0],
+        recordedTimezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        musclesWorked: muscles,
+      });
+    }
+    catch (error) {
+      console.error("Error saving workout:", error); // TODO: Handle Error
+    }
+
+    // Cleanup
+    setIsWorkingOut(false);
+    setIsModalOpen(false);
+    localStorage.clear();
+  };
+
+  // TODO: Finish log workout logic
   return (
-    <div className="flex flex-col items-center gap-4">
-      <div className="text-4xl font-mono font-bold text-white">
-        {formatTime(seconds)}
-      </div>
+    <div className="w-full items-center justfiy-center">
+      <GlowCard className="min-w-full text-center">
+        <GlowCardHeader className="pb-0">
+          {isWorkingOut ? (<WorkoutTimer/>) : ""}
+        </GlowCardHeader>
+
+        <GlowCardContent className="pt-[24px] flex flex-col justify-center items-center">
+          <CircularButton 
+            bgColor={isWorkingOut ? "white" : "none"}
+            outlineColor={isWorkingOut ? "black" : "white"}
+            className={`${isWorkingOut ? "text-black" : ""} w-50 h-50`}
+            onClick={() => toggleWorkout()}
+          >
+            {isWorkingOut ? "End" : "Workout"}
+          </CircularButton>
+
+          <GlowCardDescription className="pt-5">
+            Done your workout already? Log it
+          </GlowCardDescription>
+        </GlowCardContent>
+      </GlowCard>
+
+      <MusclesSelection
+        ref={dialogRef}
+        onConfirm={handleFinishWorkout}
+        onClose={() => setIsModalOpen(false)}
+      />
     </div>
   );
 }
