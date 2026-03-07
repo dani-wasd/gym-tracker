4a90e304299dbdea1987d48a87567c29f5d3b36d
Daniel <danielau8807@gmail.com>
2026-03-07 14:48:38 +1100

    added protection for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,16 @@
 import ScheduleSelector from "@/src/components/schedule-select";
 import LogWorkout from "@/src/components/log-workout";
+import { auth } from "@/src/lib/authHelper";
+import { getUserPublicData } from "@/src/lib/dashboardHelpers";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const user = await auth();
+  const username: string = (await getUserPublicData(user.user.id)).username;
+
   return (
     <div className="flex flex-col gap-10 items-center p-10 w-auto">
-      <LogWorkout />
+      <p>Welcome <b>{username}</b>!</p>
+      <LogWorkout userId={user.user.id} />
       <ScheduleSelector />
     </div>
   );
