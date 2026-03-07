74713884c6357ff813defc507778877c85d401f5
Daniel <danielau8807@gmail.com>
2026-03-07 15:05:01 +1100

    temp redirect to dashboard page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // TODO: Directly redirect to login if not signed in
+  // TODO: Create a root page
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
